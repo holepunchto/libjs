ee5a44a3aba67d1207e5a9649bcb0a7cf423c3b6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-12-21 11:38:58 +0100

    Add additional and adjust existing reference test

--- test/create-reference.c
+++ test/create-reference.c
@@ -9,8 +9,6 @@ main () {
 
   uv_loop_t *loop = uv_default_loop();
 
-  js_set_flags_from_string("--expose-gc", -1);
-
   js_platform_t *platform;
   e = js_create_platform(loop, &platform);
   assert(e == 0);
@@ -19,36 +17,21 @@ main () {
   e = js_create_env(loop, platform, &env);
   assert(e == 0);
 
-  js_handle_scope_t *scope;
-  e = js_open_handle_scope(env, &scope);
-  assert(e == 0);
-
   js_value_t *value;
   e = js_create_object(env, &value);
   assert(e == 0);
 
   js_ref_t *ref;
-  e = js_create_reference(env, value, 0, &ref);
-  assert(e == 0);
-
-  e = js_close_handle_scope(env, scope);
-  assert(e == 0);
-
-  js_request_garbage_collection(env);
-
-  e = js_open_handle_scope(env, &scope);
+  e = js_create_reference(env, value, 1, &ref);
   assert(e == 0);
 
   js_value_t *result;
   e = js_get_reference_value(env, ref, &result);
   assert(e == 0);
 
-  assert(result == NULL);
+  assert(result != NULL);
 
   e = js_delete_reference(env, ref);
-  assert(e == 0);
-
-  e = js_close_handle_scope(env, scope);
   assert(e == 0);
 
   e = js_destroy_env(env);
