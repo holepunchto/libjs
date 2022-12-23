43b0bf9aa7985c689b73f573e90bbcffe7797e8c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-12-23 12:31:46 +0100

    Add promise tests

---
+++ test/create-promise-reject.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, &env);
+  assert(e == 0);
+
+  js_deferred_t *deferred;
+  js_value_t *promise;
+  e = js_create_promise(env, &deferred, &promise);
+  assert(e == 0);
+
+  js_promise_state_t state;
+  e = js_get_promise_state(env, promise, &state);
+  assert(e == 0);
+
+  assert(state == js_promise_pending);
+
+  js_value_t *value;
+  e = js_create_int32(env, 42, &value);
+  assert(e == 0);
+
+  e = js_reject_deferred(env, deferred, value);
+  assert(e == 0);
+
+  e = js_get_promise_state(env, promise, &state);
+  assert(e == 0);
+
+  assert(state == js_promise_rejected);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
