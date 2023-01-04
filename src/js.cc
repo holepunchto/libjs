c1a9516ec0587c7a34450904220882508d1282a6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-01-04 16:21:41 +0100

    Fix bad references

--- src/js.cc
+++ src/js.cc
@@ -1846,7 +1846,7 @@ js_get_value_string_utf8 (js_env_t *env, js_value_t *value, char *str, size_t le
   if (str == nullptr) {
     *result = local->Utf8Length(env->isolate);
   } else if (len != 0) {
-    int written = local->WriteUtf8(
+    len = local->WriteUtf8(
       env->isolate,
       str,
       len - 1,
@@ -1854,7 +1854,7 @@ js_get_value_string_utf8 (js_env_t *env, js_value_t *value, char *str, size_t le
       String::REPLACE_INVALID_UTF8 | String::NO_NULL_TERMINATION
     );
 
-    str[written] = '\0';
+    str[len] = '\0';
 
     if (result != nullptr) {
       *result = len;
