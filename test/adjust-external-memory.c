49eec46ff38992cb0c313e9537f710e7fa595fcf
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-01-10 11:46:14 +0100

    Add external memory adjustment test

---
+++ test/adjust-external-memory.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, &env);
+  assert(e == 0);
+
+  int64_t external_memory = 0;
+
+  e = js_adjust_external_memory(env, 100, &external_memory);
+  assert(e == 0);
+
+  assert(external_memory == 100);
+
+  e = js_adjust_external_memory(env, -50, &external_memory);
+  assert(e == 0);
+
+  assert(external_memory == 50);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
