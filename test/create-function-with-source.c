27dd14d9176f3f3d9af279da6ba3ed3668c39f72
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-22 13:05:25 +0100

    Add simple `js_create_function_with_source()` test

---
+++ test/create-function-with-source.c
@@ -0,0 +1,66 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, &env);
+  assert(e == 0);
+
+  js_value_t *args[1];
+
+  e = js_create_string_utf8(env, "n", -1, &args[0]);
+  assert(e == 0);
+
+  js_value_t *source;
+  e = js_create_string_utf8(env, "return n * 2", -1, &source);
+  assert(e == 0);
+
+  js_value_t *fn;
+  e = js_create_function_with_source(env, "hello", -1, "test.js", -1, args, 1, 0, source, &fn);
+  assert(e == 0);
+
+  js_value_t *global;
+  e = js_get_global(env, &global);
+  assert(e == 0);
+
+  e = js_set_named_property(env, global, "hello", fn);
+  assert(e == 0);
+
+  js_value_t *script;
+  e = js_create_string_utf8(env, "hello(42)", -1, &script);
+  assert(e == 0);
+
+  js_value_t *result;
+  e = js_run_script(env, NULL, 0, 0, script, &result);
+  assert(e == 0);
+
+  bool has_exception;
+  e = js_is_exception_pending(env, &has_exception);
+  assert(e == 0);
+
+  assert(!has_exception);
+
+  uint32_t value;
+  js_get_value_uint32(env, result, &value);
+  assert(e == 0);
+
+  assert(value == 84);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
