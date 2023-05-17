039bb981483e0f6092290dae19a01adfaa33fc48
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-05-17 10:42:59 +0200

    Add more string tests

---
+++ test/create-string-utf8.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, &env);
+  assert(e == 0);
+
+  js_value_t *string;
+  e = js_create_string_utf8(env, "hello world", 11, &string);
+  assert(e == 0);
+
+  size_t len;
+  e = js_get_value_string_utf8(env, string, NULL, 0, &len);
+  assert(e == 0);
+
+  assert(len == 11);
+
+  char value[11];
+  e = js_get_value_string_utf8(env, string, value, len, &len);
+  assert(e == 0);
+
+  assert(len == 11);
+
+  assert(memcmp(value, "hello world", len) == 0);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
