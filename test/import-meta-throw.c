e7881e25aeef345910180750ac873c3bc8b27f3d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-06-20 10:58:23 +0200

    Add missing file

---
+++ test/import-meta-throw.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+#include <utf.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+static void
+on_module_meta (js_env_t *env, js_module_t *module, js_value_t *meta, void *data) {
+  js_throw_error(env, NULL, "err");
+}
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, NULL, &env);
+  assert(e == 0);
+
+  js_value_t *source;
+  e = js_create_string_utf8(env, (utf8_t *) "import.meta", -1, &source);
+  assert(e == 0);
+
+  js_module_t *module;
+  e = js_create_module(env, "test.js", -1, 0, source, on_module_meta, NULL, &module);
+  assert(e == 0);
+
+  e = js_instantiate_module(env, module, NULL, NULL);
+  assert(e == 0);
+
+  js_value_t *promise;
+  e = js_run_module(env, module, &promise);
+  assert(e == 0);
+
+  js_promise_state_t state;
+  e = js_get_promise_state(env, promise, &state);
+  assert(e == 0);
+
+  assert(state == js_promise_rejected);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
