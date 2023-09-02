126e61440df07a02fd68dbad6152223ef1a45820
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-09-02 18:25:52 +0200

    Add platform identifier and version tests

---
+++ test/get-platform-identifier.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  const char *identifier;
+  e = js_get_platform_identifier(platform, &identifier);
+  assert(e == 0);
+
+  printf("identifier=%s\n", identifier);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
---
+++ test/get-platform-version.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  const char *version;
+  e = js_get_platform_version(platform, &version);
+  assert(e == 0);
+
+  printf("version=%s\n", version);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+}
