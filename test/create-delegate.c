bc9c5c2f2f908c83221e92a717be7d82459f5488
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-09-13 08:39:27 +0200

    Test delegate `delete_property` and `own_keys`

--- test/create-delegate.c
+++ test/create-delegate.c
@@ -8,6 +8,8 @@
 bool get_called = false;
 bool has_called = false;
 bool set_called = false;
+bool delete_called = false;
+bool keys_called = false;
 
 js_value_t *
 get (js_env_t *env, js_value_t *property, void *data) {
@@ -64,6 +66,41 @@ set (js_env_t *env, js_value_t *property, js_value_t *value, void *data) {
   return true;
 }
 
+bool
+delete_property (js_env_t *env, js_value_t *property, void *data) {
+  int e;
+
+  delete_called = true;
+
+  utf8_t name[4];
+  e = js_get_value_string_utf8(env, property, name, 4, NULL);
+  assert(e == 0);
+
+  assert(strcmp((char *) name, "foo") == 0);
+
+  return true;
+}
+
+js_value_t *
+own_keys (js_env_t *env, void *data) {
+  int e;
+
+  keys_called = true;
+
+  js_value_t *result;
+  e = js_create_array_with_length(env, 1, &result);
+  assert(e == 0);
+
+  js_value_t *property;
+  e = js_create_string_utf8(env, (utf8_t *) "foo", -1, &property);
+  assert(e == 0);
+
+  e = js_set_element(env, result, 0, property);
+  assert(e == 0);
+
+  return result;
+}
+
 int
 main () {
   int e;
@@ -82,6 +119,8 @@ main () {
     get,
     has,
     set,
+    delete_property,
+    own_keys,
   };
 
   js_value_t *delegate;
@@ -147,6 +186,56 @@ main () {
     assert(e == 0);
 
     assert(value == 42);
+  }
+
+  e = js_create_string_utf8(env, (utf8_t *) "delete delegate.foo", -1, &script);
+  assert(e == 0);
+
+  e = js_run_script(env, NULL, 0, 0, script, &result);
+  assert(e == 0);
+
+  assert(delete_called);
+
+  {
+    bool value;
+    e = js_get_value_bool(env, result, &value);
+    assert(e == 0);
+
+    assert(value == true);
+  }
+
+  e = js_create_string_utf8(env, (utf8_t *) "const keys = []; for (const key in delegate) keys.push(key); keys", -1, &script);
+  assert(e == 0);
+
+  e = js_run_script(env, NULL, 0, 0, script, &result);
+  assert(e == 0);
+
+  assert(keys_called);
+
+  {
+    bool is_array;
+    e = js_is_array(env, result, &is_array);
+    assert(e == 0);
+
+    assert(is_array);
+
+    uint32_t len;
+    e = js_get_array_length(env, result, &len);
+    assert(e == 0);
+
+    assert(len == 1);
+
+    for (size_t i = 0; i < len; i++) {
+      js_value_t *value;
+      e = js_get_element(env, result, i, &value);
+      assert(e == 0);
+
+      bool is_string;
+      e = js_is_string(env, value, &is_string);
+      assert(e == 0);
+
+      assert(is_string);
+    }
   }
 
   e = js_destroy_env(env);
