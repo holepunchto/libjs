bcb5a9ea9135a9be7fd902f4e9365dc12524cd7b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-09-19 16:17:01 +0200

    Test object coercion

---
+++ test/coerce-object-to-string.c
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <string.h>
+#include <utf.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, NULL, &env);
+  assert(e == 0);
+
+  js_value_t *object;
+  e = js_create_object(env, &object);
+  assert(e == 0);
+
+  js_value_t *string;
+  e = js_coerce_to_string(env, object, &string);
+  assert(e == 0);
+
+  uint8_t value[16];
+  e = js_get_value_string_utf8(env, string, value, 16, NULL);
+  assert(e == 0);
+
+  assert(strcmp((char *) value, "[object Object]") == 0);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+
+  e = uv_run(loop, UV_RUN_DEFAULT);
+  assert(e == 0);
+}
