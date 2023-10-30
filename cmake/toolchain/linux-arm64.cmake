b2f468295e0fcf7b382db1f5ab5e80300e66f3df
Parallels <kasperisager@hey.com>
2023-10-30 19:01:56 +0100

    Add Linux toolchains

---
+++ cmake/toolchain/linux-arm64.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(target aarch64-linux-gnu)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${target})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${target})
