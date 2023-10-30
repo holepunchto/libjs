d98a00bebec138deecc3ffa6792c3ec50846c757
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-10-30 18:08:27 +0100

    Add initial Darwin toolchains

---
+++ cmake/toolchain/darwin-arm64.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
+
+set(target arm64-apple-macosx10.13.0)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${target})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${target})
---
+++ cmake/toolchain/darwin-x64.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(target x86_64-apple-macosx10.13.0)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${target})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${target})
