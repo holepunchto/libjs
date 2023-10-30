12f2f648222694024c41f43deb48e885be59a7f6
Parallels <kasperisager@hey.com>
2023-10-30 19:57:11 +0100

    Add Android toolchains

--- cmake/toolchain/linux-x64.cmake
+++ cmake/toolchain/linux-x64.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(target x86_64-pc-linux-gnu)
+set(target x86_64-linux-gnu)
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${target})
