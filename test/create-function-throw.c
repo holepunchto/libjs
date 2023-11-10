36841a4536263486ead35337c0eb886e8df6de0e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-10 10:28:29 +0100

    Add missing includes to tests

--- test/create-function-throw.c
+++ test/create-function-throw.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdbool.h>
+#include <string.h>
 #include <utf.h>
 #include <uv.h>
 
--- test/run-module-nested-import.c
+++ test/run-module-nested-import.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <string.h>
 #include <utf.h>
 #include <uv.h>
 
