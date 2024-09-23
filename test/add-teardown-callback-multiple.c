7ab821948d5f44574724972dea038c2d1e780d61
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-09-23 13:17:40 +0200

    Add initial tests

---
+++ test/add-teardown-callback-multiple.c
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <utf.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+static int teardown_called = 0;
+
+static void
+on_teardown (void *data) {
+  teardown_called++;
+
+  assert((intptr_t) data == teardown_called);
+}
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, NULL, &env);
+  assert(e == 0);
+
+  e = js_add_teardown_callback(env, on_teardown, (void *) 3);
+  assert(e == 0);
+
+  e = js_add_teardown_callback(env, on_teardown, (void *) 2);
+  assert(e == 0);
+
+  e = js_add_teardown_callback(env, on_teardown, (void *) 1);
+  assert(e == 0);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  assert(teardown_called == 3);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+
+  e = uv_run(loop, UV_RUN_DEFAULT);
+  assert(e == 0);
+}
---
+++ test/add-teardown-callback-remove-on-call.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <utf.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+static bool teardown_called = false;
+
+static void
+on_teardown (void *data) {
+  int e;
+
+  teardown_called = true;
+
+  js_env_t *env = (js_env_t *) data;
+
+  e = js_remove_teardown_callback(env, on_teardown, (void *) env);
+  assert(e == 0);
+}
+
+int
+main () {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, NULL, &env);
+  assert(e == 0);
+
+  e = js_add_teardown_callback(env, on_teardown, (void *) env);
+  assert(e == 0);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  assert(teardown_called);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+
+  e = uv_run(loop, UV_RUN_DEFAULT);
+  assert(e == 0);
+}
