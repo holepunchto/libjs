398831d1d6350d6caacc848e16432c4e1798aee6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-04-30 09:16:59 +0200

    Add additional tests

---
+++ test/get-typedarray-info-uint16array-large.c
@@ -0,0 +1,59 @@
+#include <assert.h>
+#include <utf.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+int
+main() {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, NULL, &env);
+  assert(e == 0);
+
+  js_handle_scope_t *scope;
+  e = js_open_handle_scope(env, &scope);
+  assert(e == 0);
+
+  js_value_t *script;
+  e = js_create_string_utf8(env, (utf8_t *) "new Uint16Array(1024).fill(42)", -1, &script);
+  assert(e == 0);
+
+  js_value_t *typedarray;
+  e = js_run_script(env, NULL, 0, 0, script, &typedarray);
+  assert(e == 0);
+
+  js_typedarray_type_t type;
+  uint16_t *data;
+  size_t len;
+  size_t offset;
+  e = js_get_typedarray_info(env, typedarray, &type, (void **) &data, &len, NULL, &offset);
+  assert(e == 0);
+
+  assert(type == js_uint16array);
+  assert(len == 1024);
+  assert(offset == 0);
+
+  for (int i = 0; i < len; i++) {
+    assert(data[i] == 42);
+  }
+
+  e = js_close_handle_scope(env, scope);
+  assert(e == 0);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+
+  e = uv_run(loop, UV_RUN_DEFAULT);
+  assert(e == 0);
+}
