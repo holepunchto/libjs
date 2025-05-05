e991f647bb309ee7d7847f84f8f1b352ca89dcf2
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-05-05 12:29:16 +0200

    Remove explicit scope from `DataView` test as well

--- test/create-typed-function-with-dataview.c
+++ test/create-typed-function-with-dataview.c
@@ -19,10 +19,6 @@ on_typed_call(js_value_t *receiver, js_value_t *dataview, js_typed_callback_info
   e = js_get_typed_callback_info(info, &env, NULL);
   assert(e == 0);
 
-  js_handle_scope_t *scope;
-  e = js_open_handle_scope(env, &scope);
-  assert(e == 0);
-
   uint8_t *data;
   size_t len;
   e = js_get_dataview_info(env, dataview, (void **) &data, &len, NULL, NULL);
@@ -33,9 +29,6 @@ on_typed_call(js_value_t *receiver, js_value_t *dataview, js_typed_callback_info
   assert(data[1] == 2);
   assert(data[2] == 3);
   assert(data[3] == 4);
-
-  e = js_close_handle_scope(env, scope);
-  assert(e == 0);
 
   return 42;
 }
