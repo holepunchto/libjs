268d12feb4a887a0c6e9e2b39f187009e44219ee
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-04-30 18:36:27 +0200

    Test function that returns a double

---
+++ test/create-function-return-double.c
@@ -0,0 +1,65 @@
+#include <assert.h>
+#include <utf.h>
+#include <uv.h>
+
+#include "../include/js.h"
+
+static js_value_t *
+on_call(js_env_t *env, js_callback_info_t *info) {
+  int e;
+
+  js_value_t *result;
+  e = js_create_double(env, 3.14, &result);
+  assert(e == 0);
+
+  return result;
+}
+
+int
+main() {
+  int e;
+
+  uv_loop_t *loop = uv_default_loop();
+
+  js_platform_t *platform;
+  e = js_create_platform(loop, NULL, &platform);
+  assert(e == 0);
+
+  js_env_t *env;
+  e = js_create_env(loop, platform, NULL, &env);
+  assert(e == 0);
+
+  js_handle_scope_t *scope;
+  e = js_open_handle_scope(env, &scope);
+  assert(e == 0);
+
+  js_value_t *fn;
+  e = js_create_function(env, "createDouble", -1, on_call, NULL, &fn);
+  assert(e == 0);
+
+  js_value_t *global;
+  e = js_get_global(env, &global);
+  assert(e == 0);
+
+  js_value_t *result;
+  e = js_call_function(env, global, fn, 0, NULL, &result);
+  assert(e == 0);
+
+  double value;
+  e = js_get_value_double(env, result, &value);
+  assert(e == 0);
+
+  assert(value == 3.14);
+
+  e = js_close_handle_scope(env, scope);
+  assert(e == 0);
+
+  e = js_destroy_env(env);
+  assert(e == 0);
+
+  e = js_destroy_platform(platform);
+  assert(e == 0);
+
+  e = uv_run(loop, UV_RUN_DEFAULT);
+  assert(e == 0);
+}
